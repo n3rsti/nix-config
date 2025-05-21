875a458bfe4843267beca22f05468ca053fcde61
Krzysztof Witucki <n3rsti@protonmail.com>
2025-05-21 17:50:54 +0200

    update

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -2,32 +2,34 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, inputs, ... }:
-{
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ../../modules/nixos/main-user.nix
-      inputs.home-manager.nixosModules.default
-    ];
+{ config, pkgs, inputs, ... }: {
+  imports = [ # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ../../modules/nixos/main-user.nix
+    inputs.home-manager.nixosModules.default
+  ];
 
+  services.resolved.enable = true;
+  services.mullvad-vpn.enable = true;
+  services.mullvad-vpn.package = pkgs.mullvad-vpn;
 
-services.resolved.enable = true;
-services.mullvad-vpn.enable = true;
-services.mullvad-vpn.package = pkgs.mullvad-vpn;
-
-home-manager = {
-  # also pass inputs to home-manager modules
-  extraSpecialArgs = {inherit inputs;};
-  users = {
-    "n3rsti" = import ./home.nix;
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
   };
-};
+
+  programs.gamemode.enable = true;
+
+  home-manager = {
+    # also pass inputs to home-manager modules
+    extraSpecialArgs = { inherit inputs; };
+    users = { "n3rsti" = import ./home.nix; };
+  };
 
   main-user.enable = true;
   main-user.userName = "n3rsti";
   programs.zsh.enable = true;
-
 
   home-manager.backupFileExtension = "backup";
 
@@ -49,9 +51,12 @@ home-manager = {
     enable = true;
     xwayland.enable = true;
   };
+
   environment.sessionVariables = {
     #WLR_NO_HARDWARE_CURSOR = "1";
     NIXOS_OZONE_WL = "1";
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS =
+      "\${HOME}/.steam/root/compatibilitytools.d";
   };
   # Set your time zone.
   time.timeZone = "Europe/Warsaw";
@@ -73,7 +78,7 @@ home-manager = {
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-  services.xserver.videoDrivers = ["nvidia"];
+  services.xserver.videoDrivers = [ "nvidia" ];
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
@@ -95,35 +100,35 @@ home-manager = {
 
     nvidia = {
 
-    # Modesetting is required.
-    modesetting.enable = true;
+      # Modesetting is required.
+      modesetting.enable = true;
 
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-    # of just the bare essentials.
-    powerManagement.enable = false;
+      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+      # Enable this if you have graphical corruption issues or application crashes after waking
+      # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+      # of just the bare essentials.
+      powerManagement.enable = false;
 
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
+      # Fine-grained power management. Turns off GPU when not in use.
+      # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+      powerManagement.finegrained = false;
 
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    # Only available from driver 515.43.04+
-    open = true;
+      # Use the NVidia open source kernel module (not to be confused with the
+      # independent third-party "nouveau" open source driver).
+      # Support is limited to the Turing and later architectures. Full list of
+      # supported GPUs is at:
+      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+      # Only available from driver 515.43.04+
+      open = true;
 
-    # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
-    nvidiaSettings = true;
+      # Enable the Nvidia settings menu,
+      # accessible via `nvidia-settings`.
+      nvidiaSettings = true;
 
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+      # Optionally, you may need to select the appropriate driver version for your specific GPU.
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
   };
-};
   # Enable sound with pipewire.
   security.rtkit.enable = true;
   services.pipewire = {
@@ -147,9 +152,10 @@ home-manager = {
     isNormalUser = true;
     description = "n3rsti";
     extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-    #  thunderbird
-    ];
+    packages = with pkgs;
+      [
+        #  thunderbird
+      ];
   };
 
   # Install firefox.
@@ -158,16 +164,19 @@ home-manager = {
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  programs.appimage.enable = true;
+  programs.appimage.binfmt = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-	pkgs.neovim
-	pkgs.kitty
-	pkgs.rofi-wayland
-	pkgs.firefox
-	pkgs.hyprpaper
-	pkgs.signal-desktop
+    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    pkgs.neovim
+    pkgs.kitty
+    pkgs.rofi-wayland
+    pkgs.firefox
+    pkgs.hyprpaper
+    pkgs.signal-desktop
     pkgs.gcc
     pkgs.gopls
     pkgs.stylua
@@ -193,19 +202,36 @@ home-manager = {
     pkgs.gimp
     pkgs.zed-editor-fhs
     pkgs.go
+    pkgs.dmidecode
+    pkgs.protonup
+    pkgs.legcord
+    pkgs.gnumake
+    pkgs.glew
+    pkgs.glfw
+    pkgs.pkg-config
+    pkgs.glm
+    pkgs.bear
+    pkgs.package-version-server
+    pkgs.nil
+    pkgs.nixfmt-classic
+    pkgs.path-of-building
+    pkgs.lutris
+    pkgs.wget
+    pkgs.obs-studio
+    pkgs.prismlauncher
   ];
 
-    fonts.packages = with pkgs; [
-        pkgs.nerd-fonts.iosevka
-        pkgs.font-awesome
-        pkgs.nerd-fonts.space-mono
-        pkgs.nerd-fonts.zed-mono
-        pkgs.nerd-fonts.im-writing
-        pkgs.nerd-fonts.fira-code
-        pkgs.nerd-fonts.fira-mono
-    ];
+  fonts.packages = with pkgs; [
+    pkgs.nerd-fonts.iosevka
+    pkgs.font-awesome
+    pkgs.nerd-fonts.space-mono
+    pkgs.nerd-fonts.zed-mono
+    pkgs.nerd-fonts.im-writing
+    pkgs.nerd-fonts.fira-code
+    pkgs.nerd-fonts.fira-mono
+  ];
 
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
