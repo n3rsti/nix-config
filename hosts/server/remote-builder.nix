d5d5d95b9733216e318380aad8e7067daed90515
Krzysztof Witucki <n3rsti@protonmail.com>
2026-01-13 14:16:25 +0100

    update distributed builds

--- hosts/server/remote-builder.nix
+++ hosts/server/remote-builder.nix
@@ -1,11 +1,28 @@
 {
   users.users.remotebuild = {
-    isSystemUser = true;
+    isNormalUser = true;
+    createHome = false;
     group = "remotebuild";
-    useDefaultShell = true;
   };
 
   users.groups.remotebuild = { };
 
-  nix.settings.trusted-users = [ "remotebuild" ];
+  nix = {
+    nrBuildUsers = 64;
+    settings = {
+      trusted-users = [ "remotebuild" ];
+
+      min-free = 10 * 1024 * 1024;
+      max-free = 200 * 1024 * 1024;
+
+      max-jobs = "auto";
+      cores = 0;
+    };
+  };
+
+  systemd.services.nix-daemon.serviceConfig = {
+    MemoryAccounting = true;
+    MemoryMax = "90%";
+    OOMScoreAdjust = 500;
+  };
 }
