e8eb9d690be65c347b7037d9203b2b0a474836d7
Krzysztof Witucki <n3rsti@protonmail.com>
2026-06-14 19:18:01 +0200

    feat: fix fractional scaling in firefox

--- hosts/pc/home.nix
+++ hosts/pc/home.nix
@@ -8,4 +8,6 @@
     path = "${config.home.homeDirectory}/.ssh/id_ed25519";
     mode = "0600";
   };
+
+  programs.firefox.profiles.default.settings."widget.wayland.fractional-scale.enabled" = false;
 }
